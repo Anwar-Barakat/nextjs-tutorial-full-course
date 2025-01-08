8fe33c32a18eef5ae8bcfb05c776f903d20ccd06
Anwar-Barakat <brkatanwar0@gmail.com>
2025-01-08 22:53:22 +0400

    Add RefComponent to Shopping page

--- app/shopping/page.tsx
+++ app/shopping/page.tsx
@@ -1,10 +1,12 @@
 import ReducerComponent from '@/components/custom/ReducerComponent'
+import RefComponent from '@/components/custom/RefComponent'
 import React from 'react'
 
 const Shopping = () => {
     return (
         <div>
-            <ReducerComponent />
+            {/* <ReducerComponent /> */}
+            <RefComponent />
         </div>
     )
 }
